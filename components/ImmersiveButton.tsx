53a4f62cd125a684f2481c2ff27683653d8db849
wuzhozho <wuzhizhon8@gmail.com>
2024-07-04 21:35:13 +0800

    沉浸式完成

--- components/ImmersiveButton.tsx
+++ components/ImmersiveButton.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from 'styled-components';
 import { Mic as MicIcon, MicOff as MicOffIcon, MessageCircle as MessageCircleIcon } from "lucide-react"; 
 
@@ -9,13 +9,11 @@ interface ActionButtonsProps {
     onMicrophoneClick: () => void;
 }
 
-interface AnimatedMicOffProps extends React.SVGProps<SVGSVGElement> {
+interface AnimatedMicIconProps extends React.SVGProps<SVGSVGElement> {
     isRecording: boolean;
-  }
+}
 
-const AnimatedMicOff = styled(MicOffIcon).attrs<AnimatedMicOffProps>(props => ({
-  ...props, // 将所有属性传递给 MicOffIcon
-}))<AnimatedMicOffProps>`
+const AnimatedMicOff = styled(MicOffIcon)<AnimatedMicIconProps>`
   animation: ${props => props.isRecording ? 'bounce 0.5s infinite alternate' : 'none'};
 
   @keyframes bounce {
@@ -29,14 +27,26 @@ const ImmersiveButton: React.FC<ActionButtonsProps> = ({
     onGoBack,
     onMicrophoneClick,
 }) => {
+    const [isMicOn, setIsMicOn] = useState(true); // 初始状态：麦克风打开
+
+    const handleMicrophoneClick = () => {
+        setIsMicOn(!isMicOn); // 切换麦克风状态
+        onMicrophoneClick();  // 执行传入的点击处理函数
+    };
+
     return (
         <div style={{ position: 'fixed', bottom: 20, left: 100, width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center', zIndex: 100 }}>
             <div>
                 <button style={{ height: '40px', width: '40px', borderRadius: '9999px', color: 'white', border: '1px solid white'}} onClick={onGoBack}>
-                    <MessageCircleIcon />
+                    <MessageCircleIcon color="black" /> 
                 </button>
-                <button style={{ height: '50px', width: '50px', borderRadius: '9999px', backgroundColor: '#ef4444', color: 'white', fontFamily: 'monospace' }} onClick={onMicrophoneClick}>
-                <AnimatedMicOff isRecording={isRecording} style={{ backgroundColor: 'transparent' }} /> 
+                <button style={{ height: '50px', width: '50px', borderRadius: '9999px', backgroundColor: '#ef4444', color: 'white', fontFamily: 'monospace' }} onClick={handleMicrophoneClick}>
+                    {/* 根据 isMicOn 状态渲染不同的图标 */}
+                    {isMicOn ? (
+                        <MicIcon style={{ backgroundColor: 'transparent' }} />
+                    ) : (
+                        <AnimatedMicOff isRecording={isRecording} style={{ backgroundColor: 'transparent' }} />
+                    )}
                 </button>
             </div>
         </div>
